e997f3d533b9b03da73f885274c576bdbe4201c5
Priyam Bhattacharya <itspriyambhattacharya@gmail.com>
2026-03-14 20:18:56 +0530

    added componenets radio button, checkbox and slider

--- app.py
+++ app.py
@@ -20,3 +20,38 @@ st.error("Repeating")
 # chapter 1 ends
 
 # chapter 2 starts
+
+if st.button("Clicked"):
+    st.success(f"Button Clicked")
+
+
+radio_btn = st.radio(
+    "Class",
+    [1, 2, 3, 4]
+)
+
+st.write(f"Class: {radio_btn}")
+
+st.write("Vehicle owned")
+car = st.checkbox(
+    "Car"
+)
+bike = st.checkbox(
+    "Bike"
+)
+
+if car:
+    st.success("Owns Car")
+    st.error("No Bike")
+
+if bike:
+    st.success("No Bike")
+    st.error("Owns Car")
+
+age = st.slider(
+    "Age",
+    min_value=6,
+    max_value=50,
+    value=21
+)
+st.write(f"The age is: {age}")
